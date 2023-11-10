07e0c309a280b2ef4a69add15fd60592a9d49e56
Bishal Adhikari <99350850+dearbishal@users.noreply.github.com>
2023-11-10 14:45:56 -0600

    Fixing some errors

--- controllers/Book.js
+++ controllers/Book.js
@@ -61,3 +61,36 @@ exports.Book_create_post = async function (req, res) {
         res.send(`{"error": ${err}}`);
     }
 };
+
+// for a specific Book.
+exports.Book_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await Book.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+    };
+
+    //Handle Book update form on PUT.
+    exports.Book_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body
+    ${JSON.stringify(req.body)}`)
+    try {
+    let toUpdate = await Book.findById( req.params.id)
+    // Do updates of properties
+    if(req.body.Book_name)
+    toUpdate.Book_name = req.body.Book_name;
+    if(req.body.Book_year) toUpdate.Book_year = req.body.Book_year;
+    if(req.body.Book_price) toUpdate.Book_price = req.body.Book_price;
+    let result = await toUpdate.save();
+    console.log("Sucess " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": ${err}: Update for id ${req.params.id}
+    failed`);
+    }
+    }
