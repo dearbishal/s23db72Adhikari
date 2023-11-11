dde8e985d03366d5334100076c0f1baf64473159
Bishal Adhikari <99350850+dearbishal@users.noreply.github.com>
2023-11-10 22:52:13 -0600

    implemnt put function to update book data

--- routes/Book.js
+++ routes/Book.js
@@ -8,6 +8,9 @@ router.get('/detail', Book_controlers.Book_view_one_Page);
 /* GET create Book page */
 router.get('/create', Book_controlers.Book_create_Page);
 
+/* GET create update page */
+router.get('/update', Book_controlers.Book_update_Page);
+
 /* GET Book */
 router.get('/', Book_controlers.Book_view_all_Page );
 module.exports = router;
