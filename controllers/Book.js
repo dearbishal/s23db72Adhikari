2dbecbfcff6e6a43a623ba089b18024655d3bdee
Bishal Adhikari <99350850+dearbishal@users.noreply.github.com>
2023-11-10 21:58:03 -0600

    Books edit

--- controllers/Book.js
+++ controllers/Book.js
@@ -7,18 +7,68 @@ exports.Book_list = async function (req, res) {
 exports.Book_detail = function (req, res) {
     res.send('NOT IMPLEMENTED: Book detail: ' + req.params.id);
 };
+// Handle a show one view with id specified by query
+exports.Book_view_one_Page = async function (req, res) {
+    console.log("single view for id " + req.query.id)
+    try {
+        result = await Book.findById(req.query.id)
+        res.render('BookDetail',
+            { title: 'Book Detail', toShow: result });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+
 // Handle Book create on POST.
-exports.Book_create_post = function (req, res) {
-    res.send('NOT IMPLEMENTED: Book create POST');
+exports.Book_create_post = async function (req, res) {
+    console.log(req.body)
+    let document = new Book();
+    document.Book_name = req.body.Book_name;
+    document.Book_year = req.body.Book_year;
+    document.Book_price = req.body.Book_price;
+    try {
+        let result = await document.save();
+        res.send(result);
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
 };
 // Handle Book delete form on DELETE.
-exports.Book_delete = function (req, res) {
-    res.send('NOT IMPLEMENTED: Book delete DELETE ' + req.params.id);
+exports.Book_delete = async function (req, res) {
+    console.log("delete " + req.params.id)
+    try {
+        result = await Book.findByIdAndDelete(req.params.id)
+        console.log("Removed " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": Error deleting ${err}}`);
+    }
 };
 // Handle Book update form on PUT.
-exports.Book_update_put = function (req, res) {
-    res.send('NOT IMPLEMENTED: Book update PUT' + req.params.id);
-};
+exports.Book_update_put = async function (req, res) {
+    console.log(`update on id ${req.params.id} with body
+        ${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await Book.findById(req.params.id)
+        // Do updates of properties
+        if (req.body.Book_name)
+            toUpdate.Book_name = req.body.Book_name;
+        if (req.body.Book_year) toUpdate.Book_year = req.body.Book_year;
+        if (req.body.Book_price) toUpdate.Book_price = req.body.Book_price;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id}
+        failed`);
+    }
+}
 
 // List of all Book
 exports.Book_list = async function (req, res) {
@@ -44,53 +94,14 @@ exports.Book_view_all_Page = async function (req, res) {
         res.send(`{"error": ${err}}`);
     }
 };
-
-// Handle Books create on POST.
-exports.Book_create_post = async function (req, res) {
-    console.log(req.body)
-    let document = new Book();
-    document.Book_name = req.body.Book_name;
-    document.Book_year = req.body.Book_year;
-    document.Book_price = req.body.Book_price;
-    try {
-        let result = await document.save();
-        res.send(result);
-    }
-    catch (err) {
-        res.status(500);
-        res.send(`{"error": ${err}}`);
-    }
-};
-
 // for a specific Book.
-exports.Book_detail = async function(req, res) {
+exports.Book_detail = async function (req, res) {
     console.log("detail" + req.params.id)
     try {
-    result = await Book.findById( req.params.id)
-    res.send(result)
+        result = await Book.findById(req.params.id)
+        res.send(result)
     } catch (error) {
-    res.status(500)
-    res.send(`{"error": document for id ${req.params.id} not found`);
+        res.status(500)
+        res.send(`{"error": document for id ${req.params.id} not found`);
     }
-    };
-
-    //Handle Book update form on PUT.
-    exports.Book_update_put = async function(req, res) {
-    console.log(`update on id ${req.params.id} with body
-    ${JSON.stringify(req.body)}`)
-    try {
-    let toUpdate = await Book.findById( req.params.id)
-    // Do updates of properties
-    if(req.body.Book_name)
-    toUpdate.Book_name = req.body.Book_name;
-    if(req.body.Book_year) toUpdate.Book_year = req.body.Book_year;
-    if(req.body.Book_price) toUpdate.Book_price = req.body.Book_price;
-    let result = await toUpdate.save();
-    console.log("Sucess " + result)
-    res.send(result)
-    } catch (err) {
-    res.status(500)
-    res.send(`{"error": ${err}: Update for id ${req.params.id}
-    failed`);
-    }
-    }
+};
--- routes/Book.js
+++ routes/Book.js
@@ -1,6 +1,12 @@
 var express = require('express');
 const Book_controlers= require('../controllers/Book');
 var router = express.Router();
-/* GET costumes */
+
+/* GET detail costume page */
+router.get('/detail', Book_controlers.Book_view_one_Page);
+
+
+/* GET Book */
 router.get('/', Book_controlers.Book_view_all_Page );
 module.exports = router;
+
